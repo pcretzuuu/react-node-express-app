afc231f186f12378ee4f185dad90477090ddba81
Claudiu <claudiupalalaeiulian@gmail.com>
2019-12-19 09:41:14 +0200

    first-commit

---
+++ build/precache-manifest.015281c62de9e7762c567936e908a70f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92582ca8567184679d1f3d2e6986d5bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f9ef984a0743bd03de8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "394d57b5c949e1c810a4",
+    "url": "/static/js/2.d4de7213.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d4de7213.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f9ef984a0743bd03de8",
+    "url": "/static/js/main.346ba1fc.chunk.js"
+  },
+  {
+    "revision": "fea54fc723b43a097b38",
+    "url": "/static/js/runtime-main.f4fc2182.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
